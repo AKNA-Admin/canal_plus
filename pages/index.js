156f686f52b1a303249b4cc585b6f78377c570a0
AKNA-Admin <akna-ci@outlook.fr>
2026-08-02 21:30:53 +0000

    Update logo image styles in index.js
    Removed borderRadius style from the logo image.

--- pages/index.js
+++ pages/index.js
@@ -96,7 +96,7 @@ export default function FormulaireVente() {
   <img 
     src="https://res.cloudinary.com/zpy0qbjp/image/upload/v1785704386/Picture1_ekbv8d.png"
     alt="Logo Canal+ PDV" 
-    style={{ Width: '12Opx', height: 'auto', display: 'block', margin: '0 auto', borderRadius: '4px' }} 
+    style={{ Width: '12Opx', height: 'auto', display: 'block', margin: '0 auto' }} 
    />
    </div>
       
