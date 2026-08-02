a0f26497ce80b9ef7eb7ebecd2fce6ffce3f793c
AKNA-Admin <akna-ci@outlook.fr>
2026-08-02 21:42:45 +0000

    Adjust margin-bottom in centered div
    Increased margin-bottom for the centered div.

--- pages/index.js
+++ pages/index.js
@@ -92,7 +92,7 @@ export default function FormulaireVente() {
 
   return (
     <div style={{ padding: '30px', maxWidth: '500px', margin: '40px auto', fontFamily: 'Segoe UI, sans-serif', boxShadow: '0 4px 15px rgba(0,0,0,0.1)', borderRadius: '8px', backgroundColor: '#fff' }}>
-    <div style={{ textAlign: 'center', marginBottom: '15px' }}>
+    <div style={{ textAlign: 'center', marginBottom: '20px' }}>
   <img 
     src="https://res.cloudinary.com/zpy0qbjp/image/upload/v1785704386/Picture1_ekbv8d.png"
     alt="Logo Canal+ PDV" 
