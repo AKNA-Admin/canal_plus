6249f5a9b9e94392020dba72172a0c180f8fa088
AKNA-Admin <akna-ci@outlook.fr>
2026-08-06 18:11:32 +0000

    Update Navbar.tsx

--- visual/src/components/Navbar.tsx
+++ visual/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <header className="bg-black text-white p-4 flex items-center gap-3 shadow-lg">
-      <img src="/logo-canal.png" alt="Canal+" className="h-8" /> {/* AJOUTE ÇA */}
+      <img src="https://res.cloudinary.com/zpy0qbjp/image/upload/v1786037223/icon_my_App_PDV_dekfzu.jpg" alt="Canal+" className="h-8" /> {/* AJOUTE ÇA */}
       <h1 className="text-xl font-bold">PDV Manager</h1>
       <div className="ml-auto">
         {/* Bouton logout etc */}
